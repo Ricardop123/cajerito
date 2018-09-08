512d87772e757c58c72be0f9472c73651d2ff223
Ricardop123 <i.ra@hotmail.com>
2018-09-08 14:12:34 -0500

    today progress

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import ViewAllBalances
+from django.urls import re_path, path
+from .views import ViewAllBalances, WithdrawAction, WithdrawMoney
 
-urlpatterns = {
-	path('',ViewAllBalances.as_view())
-}
+app_name = 'accounts'
+urlpatterns = [
+	path('',ViewAllBalances.as_view(), name='main'),
+	re_path(r'^msg/(?P<msg>\w+)/$', ViewAllBalances.as_view(), name='main'),
+	path('withdraw_action/', WithdrawAction.as_view(), name='withdraw_redirect'),
+	path('withdraw/', WithdrawMoney.as_view(), name='withdraw'),
+]
