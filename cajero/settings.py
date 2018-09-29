f3a23e76ad30a19a06d7a47861a94b34881b127a
Ricardop123 <i.ra@hotmail.com>
2018-09-29 13:34:31 -0500

    release 1

--- cajero/settings.py
+++ cajero/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'node')]
+    os.path.join(BASE_DIR, 'node_modules'),
+    os.path.join(BASE_DIR, 'static'),
+]
